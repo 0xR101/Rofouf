2be66d7f235309905b57a0fb6ffaa4c328e9af11
ayed87 <gake12234@gmail.com>
2024-04-07 07:47:53 +0300

    split the home screen form the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import "./App.css";
 import "./index.css";
-import NavBar from "./components/nav/NavBar";
-import Ad from "./components/ad/Ad.jsx";
+import Home from './pages/Home.jsx';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-// import React from "react";
-import GroupSlider from "./components/slider/slider.jsx";
-import BookCard from "./components/bookCard/BookCard.jsx";
-import Footer from './components/footer/Footer.jsx';
+
 
 
 
@@ -16,36 +10,7 @@ function App() {
   return (
     // this code will be replaced by routes for all pages, but for now we keep it like this
     <>
-      <NavBar></NavBar>
-      <div
-        style={{
-          padding: "20px",
-        }}
-      >
-        <Ad></Ad>
-        <h1>Active Groups</h1>
-
-        <div className="mt-5 bg-secondaryColor p-5 rounded-lg gap-2 relative z-0">
-          <GroupSlider />
-        </div>
-        <h1>Recommendations</h1>
-        <div className="grid   gap-3 md:grid-cols-2 sm:grid-cols-1">
-          <BookCard></BookCard>
-          <BookCard></BookCard>
-          <BookCard></BookCard>
-          <BookCard></BookCard>
-          <BookCard></BookCard>
-          <BookCard></BookCard>
-          <BookCard></BookCard>
-          <BookCard></BookCard>
-          <BookCard></BookCard>
-          <BookCard></BookCard>
-        </div>
-      </div>
-
-      <Footer></Footer>
-
-
+    <Home></Home>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,43 @@
-import NavBar from "./components/nav/NavBar";
+import NavBar from './../components/nav/NavBar.jsx';
+import Ad from './../components/ad/Ad.jsx';
+import GroupSlider from './../components/slider/slider.jsx';
+import BookCard from './../components/bookCard/BookCard.jsx';
+import Footer from './../components/footer/Footer.jsx';
 
 
-
-import Ad from "./components/ad/Ad.jsx";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-// import React from "react";
-import GroupSlider from "./components/slider/slider.jsx";
 
 function Home() {
   return (
+    // this code will be replaced by routes for all pages, but for now we keep it like this
     <>
       <NavBar></NavBar>
       <div
         style={{
-          padding: "10px",
+          padding: "20px",
         }}
       >
         <Ad></Ad>
         <h1>Active Groups</h1>
 
-        <div
-          style={{
-            marginTop: "20px",
-            backgroundColor: "#cabdb3",
-            padding: "20px",
-            borderRadius: "10px",
-            gap: "10px",
-          }}
-        >
+        <div className="mt-5 bg-secondaryColor p-5 rounded-lg gap-2 relative z-0">
           <GroupSlider />
         </div>
         <h1>Recommendations</h1>
+        <div className="grid   gap-3 md:grid-cols-2 sm:grid-cols-1">
+          <BookCard></BookCard>
+          <BookCard></BookCard>
+          <BookCard></BookCard>
+          <BookCard></BookCard>
+          <BookCard></BookCard>
+          <BookCard></BookCard>
+          <BookCard></BookCard>
+          <BookCard></BookCard>
+          <BookCard></BookCard>
+          <BookCard></BookCard>
+        </div>
       </div>
+
+      <Footer></Footer>
     </>
   );
 }
