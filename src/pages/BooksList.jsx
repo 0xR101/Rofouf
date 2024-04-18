728d7bb2d82e98276988e1ec0a1ffe686ca1f74c
0xr101 <rayyanreda31@gmail.com>
2024-04-17 22:50:03 -0700

    added books list

---
+++ src/pages/BooksList.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./../components/nav/NavBar.jsx";
+import Footer from "./../components/footer/Footer.jsx";
+
+export default function BooksList() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <div>
+        <h1>Book list here</h1>
+      </div>
+      <h3>dropdown menu hear</h3>
+      <h3>All listed items depending on the status </h3>
+
+      <Footer></Footer>
+    </>
+  );
+}
