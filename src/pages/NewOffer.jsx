0b8e6c87aa567cf02a78df86934976d12f210ace
0xr101 <rayyanreda31@gmail.com>
2024-04-17 22:50:24 -0700

    new offer page added

---
+++ src/pages/NewOffer.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./../components/nav/NavBar.jsx";
+import Footer from "./../components/footer/Footer.jsx";
+
+export default function NewOffer() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <div>
+        <h1>Create new offer </h1>
+      </div>
+      <Footer></Footer>
+    </>
+  );
+}
