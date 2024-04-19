70f198af8f46ffda54149954e0382400169c90ba
AlawiSahel <alwisahel12@hotmail.com>
2024-04-19 10:32:40 +0300

    udates- added star component with half star support

--- src/pages/Seller.jsx
+++ src/pages/Seller.jsx
@@ -3,13 +3,23 @@ import Footer from "./../components/footer/Footer.jsx";
 import CommentsSection from "../components/CommentSection/CommentSection.jsx";
 import SellerHeader from "./../components/sellerHeader/SellerHeader.jsx";
 import HorizontalBookSlider from "../components/HorizontalBookSlider/HorizontalBookSlider.jsx";
+
+// Example seller data with an ID property
+const sellerInfo = {
+  id: 123, // Example seller ID
+  name: "Ahmad Al-ali",
+  joinDate: "2024",
+  rating: 3.8,
+  profilePic: "./src/assets/person.png", // Replace with actual image path
+};
+
 function Seller() {
   return (
     <div className=" flex bg-offWhite flex-col min-h-screen w-full">
       <NavBar></NavBar>
 
       <main className=" flex-grow m-10 space-y-5 ">
-        <SellerHeader></SellerHeader>
+        <SellerHeader seller={sellerInfo} />
         <h1 className="text-3xl font-bold mb-4 text-primary">All Offers</h1>
         <HorizontalBookSlider></HorizontalBookSlider>
         <h1 className="text-3xl font-bold mb-4 text-primary">All Comments</h1>
