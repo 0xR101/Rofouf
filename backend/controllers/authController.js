07d8b5ca679847564299161b7052768d8b33d5e6
Hayder <hmhaleissa@gmail.com>
2024-05-11 15:09:17 +0300

    authenticating users in the sign in page

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,7 +4,6 @@ const User = require("../database/schemas/users");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
 exports.createUser = async (req, res) => {
 	try {
 		const { name, username, email, password } = req.body;
@@ -47,7 +46,56 @@ exports.createUser = async (req, res) => {
 			listedBooks: [], // Example listed books
 		});
 
-		res.status(201).json({ message: "User created successfully",status:200 });
+		res.status(201).json({
+			message: "User created successfully",
+			status: 200,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			message: "Internal server error",
+			error: error.message,
+		});
+	}
+};
+
+exports.checkUser = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		// Check if the user exists
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+
+		// Compare passwords
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+
+		if (!isPasswordValid) {
+			return res
+				.status(401)
+				.json({ message: "Invalid email or password" });
+		}
+
+		// If the password is correct, create a JWT token
+        
+		// const token = jwt.sign(
+		// 	{
+		// 		userId: user._id,
+		// 		email: user.email,
+		// 	},
+		// 	process.env.JWT_SECRET,
+		// 	{ expiresIn: "1h" },
+		// );
+
+		res.status(200).json({
+			message: "Authentication successful",
+			// token: token,
+			// userId: user._id,
+			// expiresIn: 3600,
+		});
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.post("/signup", authController.createUser);
-
+router.post("/signin", authController.checkUser);
 
 module.exports = router;
