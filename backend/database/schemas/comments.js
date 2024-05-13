4329a586aa3e2dcf9556a1975aae318029b04838
0xr101 <rayyanreda31@gmail.com>
2024-05-13 01:32:36 -0700

    changes on the schema

---
+++ backend/database/schemas/comments.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+ image: String,
+ rating: Number,
+ userName: String,
+ content: String,
+ date: { type: Date, default: new Date()},
+});
+
+
+
+module.exports = mongoose.model("Comment", commentSchema);
