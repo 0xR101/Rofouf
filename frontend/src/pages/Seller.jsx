c3f8d696c187912af369f5f42b41d63803652f2f
AlawiSahel <alwisahel12@hotmail.com>
2024-05-15 17:19:41 +0300

    update

--- frontend/src/pages/Seller.jsx
+++ frontend/src/pages/Seller.jsx
@@ -55,7 +55,7 @@ function Seller() {
         </div>
         <div>
           <h1 className="text-3xl font-bold mb-4 text-primary">All Comments</h1>
-          <CommentsSection></CommentsSection>
+          <CommentsSection seller={sellerInfo}></CommentsSection>
         </div>
       </main>
 
