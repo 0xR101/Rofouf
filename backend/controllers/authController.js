305499dc0a0ca98ffca131adcd90585380d530af
ayed87 <gake12234@gmail.com>
2024-05-15 16:24:25 +0300

    done form update user info

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -88,27 +88,38 @@ exports.createUser = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
 	try {
-		// const { user } = req.body;
-		// console.log(user);
-		// const data = {
-		// 	name: user.name,
-		// 	email: user.email,
-		// 	phoneNumber: user.phoneNumber != null ? user.phoneNumber : "",
-		// 	address: user.address != null ? user.address : "",
-		// 	username: user.username,
-		// };
-		// await User.updateOne(
-		// 	{ username: user.username },
-		// 	{
-		// 		$set: {
-		// 			data,
-		// 		},
-		// 	},
-		// );
-		// console.log(data);
-		// res.json(data);
+		console.log("we are here");
+		const user = req.body;
+		console.log(user);
+
+		const update = {
+			name: req.body.name,
+			email: req.body.email,
+			phoneNumber: req.body.phoneNumber,
+			address: req.body.address,
+			profileImage:
+				"http://localhost:5000/images/" +
+				req.file.path.split("\\").pop(),
+		};
+
+		let doc = await User.findOneAndUpdate(
+			{ username: req.body.username },
+			update,
+		);
+
+		console.log(update);
+
+		if (doc) {
+			// User updated successfully
+			res.status(200).json({ message: "User updated successfully" });
+		} else {
+			// User not found or update failed
+			res.status(404).json({ error: "User not found or update failed" });
+		}
 	} catch (error) {
 		console.error(error);
+		// Server error
+		res.status(500).json({ error: "Internal server error" });
 	}
 };
 
@@ -117,14 +128,15 @@ exports.getUser = async (req, res) => {
 		const { username } = req.query;
 		const user = await User.findOne({ username: username });
 		const data = {
-			userName: user.name,
-			userMail: user.email,
-			userNumber: user.phoneNumber != null ? user.phoneNumber : "",
-			userAddress: user.address != null ? user.address : "",
-			userId: user.username,
+			name: user.name,
+			email: user.email,
+			phoneNumber: user.phoneNumber,
+			address: user.address,
+			username: user.username,
+			profileImage: user.profileImage,
 		};
 		console.log(data);
-		res.json(data);
+		await res.json(data);
 	} catch (error) {
 		console.error(error);
 	}
